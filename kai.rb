322b8b371f85d73f17a2c0c309ee9c0bb9b0ca81
Brett R. Toomey <brettcwx@gmail.com>
2017-11-15 21:48:43 +0100

    Update kai.rb

--- kai.rb
+++ kai.rb
@@ -3,6 +3,7 @@ class Kai < Formula
   homepage "http://docs.kai-lang.org"
   version "0.1.1"
   url "https://github.com/kai-language/kai/releases/download/#{version}/macOS-sierra.tar.gz"
+  sha256 "84a2d22d35663bdc15a87a9b67ceb05ed7e978797d68fc7b66fd6c02e1909d96"
 
   depends_on "libgit2" => :run
 
