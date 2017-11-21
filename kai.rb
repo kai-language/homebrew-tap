8781a8b39310256f29fca5b99979043398af1d4a
Brett R. Toomey <brettcwx@gmail.com>
2017-11-21 19:56:16 +0100

    Update kai.rb

--- kai.rb
+++ kai.rb
@@ -1,9 +1,9 @@
 class Kai < Formula
   desc "An expressive low level programming language"
   homepage "http://docs.kai-lang.org"
-  version "0.1.3"
+  version "0.1.4"
   url "https://github.com/kai-language/kai/releases/download/#{version}/macOS-sierra.tar.gz"
-  sha256 "c852d880a1904ae60c9f433ae43f363f0169078b0a92524ca2c57bd2974fe47a"
+  sha256 "b15f34558abc2bafe6e20815005d6ab42fdabcd7df38bebf83fa2cdf589d3804"
 
   depends_on "libgit2" => :run
 
