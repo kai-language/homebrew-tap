0ea6ce8b050543fc12f527b9f573bf98d10fc5b3
Brett R. Toomey <brettcwx@gmail.com>
2018-08-31 11:28:20 +0200

    Update kai.rb

--- kai.rb
+++ kai.rb
@@ -5,7 +5,7 @@ class Kai < Formula
   url "https://github.com/kai-language/kai/releases/download/#{version}/macOS-sierra.tar.gz"
   sha256 "e3c1c685607f4eb529ce4109536f7d7f19448692a7321a4d822870b8c87ab439"
 
-  depends_on "libgit2" => :run
+  depends_on "libgit2"
 
   def install
     bin.install "kai"
